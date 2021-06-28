abbde72d7b066be2cf32b0a3bc8244859dd29e02
Vince-Dev <vinc.brocheton@gmail.com>
2021-06-28 16:12:13 +0200

    creation repo et interfaceRepo

---
+++ src/Repositories/CarburantRepo.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using Entities.Repo;
+using InterfacesContrats.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    class CarburantRepo : RepositoryBase<Carburant>, ICarburantRepo
+    {
+        public CarburantRepo(RepoContext context) : base(context)
+        {
+
+        }
+    }
+}
