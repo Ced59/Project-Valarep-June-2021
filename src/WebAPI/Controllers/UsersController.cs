0391abc2f12f41b4c475e695ce125846a071e860
Vince-Dev <vinc.brocheton@gmail.com>
2021-06-29 13:33:32 +0200

    add Mapping to connexion

--- src/WebAPI/Controllers/UsersController.cs
+++ src/WebAPI/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Models.AuthModels;
+﻿using AutoMapper;
+using Dto;
+using Entities.Models.AuthModels;
+using InterfacesContrats.Logger;
 using InterfacesContrats.RepositoryInterfaces;
 using InterfacesContrats.UserInterfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -13,20 +16,34 @@ namespace WebApi.Controllers
     {
         private IUserService _userService;
         private IUserRepo _userRepo;
+        private IMapper _mapper;
+        private ILoggable _logger;
 
-        public UsersController(IUserService userService, IUserRepo userRepo)
+        public UsersController(IUserService userService, IUserRepo userRepo, IMapper mapper, ILoggable logger)
         {
             _userService = userService;
             _userRepo = userRepo;
+            _mapper = mapper;
+            _logger = logger;
         }
 
         [HttpPost("authenticate")]
-        public IActionResult Authenticate(AuthenticateRequest model)
+        public IActionResult Authenticate([FromBody] UserConnexionDto userConnexionDto)
         {
-            var response = _userService.Authenticate(model, _userRepo);
+            if(userConnexionDto == null)
+            {
+                _logger.LogErreur("La reception du client est null");
+                return BadRequest("Objet UserConnexionDto est null");
+            }
+
+            var user = _mapper.Map<AuthenticateRequest>(userConnexionDto);
+
+            var response = _userService.Authenticate(user, _userRepo);
 
             if (response == null)
+            {
                 return BadRequest(new { message = "Username or password is incorrect" });
+            }
 
             return Ok(response);
         }
